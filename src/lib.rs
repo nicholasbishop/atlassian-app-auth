9adc424931949d8af790515bb44a926ad663187b
Nicholas Bishop <nicholasbishop@gmail.com>
2021-09-06 13:50:53 -0400

    Add crate docstring

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+//! This is a small library for authenticating with an Atlassian API
+//! (such as the Jira API) as an Atlassian Connect App.
+//!
+//! Note that the query string hash implementation is incomplete; there
+//! are a lot of special cases that are not yet handled.
+//!
+//! Relevant documentation:
+//!
+//! - https://developer.atlassian.com/cloud/jira/platform/integrating-with-jira-cloud
+//! - https://developer.atlassian.com/cloud/jira/platform/security-for-connect-apps
+//! - https://developer.atlassian.com/cloud/jira/platform/understanding-jwt
+
 use percent_encoding::{utf8_percent_encode, AsciiSet, CONTROLS};
 use serde::Serialize;
 use sha2::Digest;
